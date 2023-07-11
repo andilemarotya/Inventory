ff7db5b605fcce3bb498b7ed97f070bfa6bf80fb
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-07-11 06:03:09 +0200

    Player Inventory

---
+++ main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@onready var player: CharacterBody3D = $Player
+@onready var inventory_interface: Control = $Player/UI/InventoryInterface
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player.toggle_inventory.connect(toggle_inventory_interface)
+	inventory_interface.set_player_inventory_data(player.inventory_data)
+
+
+func toggle_inventory_interface():
+	inventory_interface.visible = not inventory_interface.visible
+	
+	if inventory_interface.visible:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
